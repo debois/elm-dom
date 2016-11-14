ba79acd1132462251bb614943e24db6b74324aa1
Daniel Stankevich <danstn@users.noreply.github.com>
2016-11-15 10:58:19 +1100

    Upgrade to 0.18

--- src/DOM.elm
+++ src/DOM.elm
@@ -1,13 +1,22 @@
-module DOM exposing
-  ( target, offsetParent, parentElement
-  , nextSibling, previousSibling
-  , childNode, childNodes
-  , offsetWidth, offsetHeight
-  , offsetLeft, offsetTop
-  , scrollLeft, scrollTop
-  , Rectangle, boundingClientRect
-  , className
-  ) 
+module DOM
+  exposing
+    ( target
+    , offsetParent
+    , parentElement
+    , nextSibling
+    , previousSibling
+    , childNode
+    , childNodes
+    , offsetWidth
+    , offsetHeight
+    , offsetLeft
+    , offsetTop
+    , scrollLeft
+    , scrollTop
+    , Rectangle
+    , boundingClientRect
+    , className
+    )
 
 {-| You read values off the DOM by constructing a JSON decoder.
 See the `target` value for example use.
@@ -35,129 +44,134 @@ for the precise semantics of these measurements. See also
 @docs className
 -}
 
-import Json.Decode as Decode exposing ((:=), at, andThen, Decoder)
+import Json.Decode as Decode exposing (field, at, andThen, Decoder)
 
 
 {-| Get the target DOM element of an event. You will usually start with this
 decoder. E.g., to make a button which when clicked emit an Action that carries
 the width of the button:
 
-    import DOM exposing (target, offsetWidth)
+  import DOM exposing (target, offsetWidth)
 
-    myButton : Html Float
-    myButton =
-      button
-        [ on "click" (target offsetWidth) ]
-        [ text "Click me!" ]
+  myButton : Html Float
+  myButton =
+    button
+    [ on "click" (target offsetWidth) ]
+    [ text "Click me!" ]
 -}
 target : Decoder a -> Decoder a
 target decoder =
-  "target" := decoder
+  field "target" decoder
 
 
 {-| Get the offsetParent of the current element. Returns first argument if the current
 element is already the root; applies the second argument to the parent element
 if not.
 
-To do traversals of the DOM, exploit that Elm allows recursive values. 
+To do traversals of the DOM, exploit that Elm allows recursive values.
 -}
 offsetParent : a -> Decoder a -> Decoder a
 offsetParent x decoder =
   Decode.oneOf
-  [ "offsetParent" := Decode.null x
-  , "offsetParent" := decoder
-  ]
+    [ field "offsetParent" <| Decode.null x
+    , field "offsetParent" decoder
+    ]
 
 
 {-| Get the next sibling of an element.
 -}
 nextSibling : Decoder a -> Decoder a
 nextSibling decoder =
-  "nextSibling" := decoder
+  field "nextSibling" decoder
 
 
 {-| Get the previous sibling of an element.
 -}
 previousSibling : Decoder a -> Decoder a
 previousSibling decoder =
-  "previousSibling" := decoder
+  field "previousSibling" decoder
 
 
-{-| Get the parent of an element. 
+{-| Get the parent of an element.
 -}
 parentElement : Decoder a -> Decoder a
-parentElement decoder = 
-  "parentElement" := decoder
+parentElement decoder =
+  field "parentElement" decoder
 
 
-{-| Find the ith child of an element. 
+{-| Find the ith child of an element.
 -}
 childNode : Int -> Decoder a -> Decoder a
-childNode idx = 
+childNode idx =
   at [ "childNodes", toString idx ]
 
 
 {-| Get the children of an element.
 -}
 childNodes : Decoder a -> Decoder (List a)
-childNodes decoder = 
+childNodes decoder =
   let
-    loop idx xs = 
-      Decode.maybe (toString idx := decoder)
-        `andThen` (
-          Maybe.map (\x -> loop (idx+1) (x::xs))
+    loop idx xs =
+      Decode.maybe (field (toString idx) decoder)
+        |> andThen
+          (Maybe.map (\x -> loop (idx + 1) (x :: xs))
             >> Maybe.withDefault (Decode.succeed xs)
-        )
+          )
   in
-    ("childNodes" := loop 0 [])
+    (field "childNodes" <| loop 0 [])
       |> Decode.map List.reverse
 
 
 
-
--- GEOMETRY 
+-- GEOMETRY
 
 
 {-| Get the width of an element in pixels; underlying implementation
 reads `.offsetWidth`.
 -}
 offsetWidth : Decoder Float
-offsetWidth = "offsetWidth" := Decode.float
+offsetWidth =
+  field "offsetWidth" Decode.float
 
 
 {-| Get the heigh of an element in pixels. Underlying implementation
 reads `.offsetHeight`.
 -}
 offsetHeight : Decoder Float
-offsetHeight = "offsetHeight" := Decode.float
+offsetHeight =
+  field "offsetHeight" Decode.float
 
 
 {-| Get the left-offset of the element in the parent element in pixels.
 Underlying implementation reads `.offsetLeft`.
 -}
 offsetLeft : Decoder Float
-offsetLeft = "offsetLeft" := Decode.float
+offsetLeft =
+  field "offsetLeft" Decode.float
 
 
 {-| Get the top-offset of the element in the parent element in pixels.
 Underlying implementation reads `.offsetTop`.
 -}
 offsetTop : Decoder Float
-offsetTop = "offsetTop" := Decode.float
+offsetTop =
+  field "offsetTop" Decode.float
 
 
 {-| Get the amount of left scroll of the element in pixels.
 Underlying implementation reads `.scrollLeft`.
 -}
 scrollLeft : Decoder Float
-scrollLeft = "scrollLeft" := Decode.float
+scrollLeft =
+  field "scrollLeft" Decode.float
 
 
 {-| Get the amount of top scroll of the element in pixels.
 Underlying implementation reads `.scrollTop`.
 -}
 scrollTop : Decoder Float
-scrollTop = "scrollTop" := Decode.float
+scrollTop =
+  field "scrollTop" Decode.float
 
 
 {-| Types for rectangles.
@@ -177,7 +191,7 @@ based off
 
 NB! This decoder produces wrong results if a parent element is scrolled and
 does not have explicit positioning (e.g., `position: relative;`); see
-[this issue](https://github.com/debois/elm-dom/issues/4). 
+[this issue](https://github.com/debois/elm-dom/issues/4).
 
 Also note that this decoder is likely computationally expensive and may produce
 results that differ slightly from `getBoundingClientRect` in browser-dependent
@@ -191,44 +205,51 @@ presumably expensive JSON decoders.  It's 2007 forever, baby!)
 -}
 boundingClientRect : Decoder Rectangle
 boundingClientRect =
-  Decode.object3
-    (\(x, y) width height ->
+  Decode.map3
+    (\( x, y ) width height ->
       { top = y
       , left = x
       , width = width
       , height = height
-      })
+      }
+    )
     (position 0 0)
     offsetWidth
     offsetHeight
 
 
+
 {- This is what we're implementing (from the above link).
 
-    function getOffset( el ) {
-        var _x = 0;
-        var _y = 0;
-        while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-            _x += el.offsetLeft - el.scrollLeft;
-            _y += el.offsetTop - el.scrollTop;
-            el = el.offsetParent;
-        }
-        return { top: _y, left: _x };
-    }
-    var x = getOffset( document.getElementById('yourElId') ).left; )
+   function getOffset( el ) {
+     var _x = 0;
+     var _y = 0;
+     while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+       _x += el.offsetLeft - el.scrollLeft;
+       _y += el.offsetTop - el.scrollTop;
+       el = el.offsetParent;
+     }
+     return { top: _y, left: _x };
+   }
+   var x = getOffset( document.getElementById('yourElId') ).left; )
 -}
-position : Float -> Float -> Decoder (Float, Float)
+
+
+position : Float -> Float -> Decoder ( Float, Float )
 position x y =
-  Decode.object4
+  Decode.map4
     (\scrollLeft scrollTop offsetLeft offsetTop ->
-      (x + offsetLeft - scrollLeft, y + offsetTop - scrollTop))
+      ( x + offsetLeft - scrollLeft, y + offsetTop - scrollTop )
+    )
     scrollLeft
     scrollTop
     offsetLeft
     offsetTop
-  `andThen` (\(x',y') ->
-    offsetParent (x', y') (position x' y')
-  )
+    |> andThen
+      (\( x_, y_ ) ->
+        offsetParent ( x_, y_ ) (position x_ y_)
+      )
+
 
 
 -- MISC
@@ -237,7 +258,5 @@ position x y =
 {-| Get the class name(s) of an element.
 -}
 className : Decoder String
-className = 
+className =
   at [ "className" ] Decode.string
-
-
